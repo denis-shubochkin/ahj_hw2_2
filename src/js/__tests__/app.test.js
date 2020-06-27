f17d5602dc48cbdd079be32d5d71950732e7b0ef
Denis Shubochkin <zippo1095@gmail.com>
2020-06-27 19:27:59 +0300

    ahj2 jest

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,13 +1,12 @@
-import insertPic from '../app'
+import insertPic from '../app';
 
 
 test('picture added', () => {
-    const field = document.getElementById('field');
-    const cells = document.getElementsByClassName('cells');
-    const cellsCount = field.childElementCount;
-    const startPos = Math.floor(0 + Math.random() * (cellsCount - 1));
-    let currPos;
-    insertPic(startPos);
-    expect(cells[currPos].firstChild.id).toBe('goblin');
-  });
-  
+  const field = document.getElementById('field');
+  const cells = document.getElementsByClassName('cells');
+  const cellsCount = field.childElementCount;
+  const startPos = Math.floor(0 + Math.random() * (cellsCount - 1));
+  let currPos;
+  insertPic(startPos);
+  expect(cells[currPos].firstChild.id).toBe('goblin');
+});
