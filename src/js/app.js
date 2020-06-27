d3f58866d2cfc0669467186fc99e0e6294ce532c
Denis Shubochkin <zippo1095@gmail.com>
2020-06-27 18:50:11 +0300

    ahj2 lint

--- src/js/app.js
+++ src/js/app.js
@@ -1,36 +1,33 @@
 // TODO: write your code here
 
-let field = document.getElementById('field');
-let cells = document.getElementsByClassName('cells');
-let cellsCount = field.childElementCount;
-let startPos = Math.floor(0  + Math.random() * (cellsCount-1));
+const field = document.getElementById('field');
+const cells = document.getElementsByClassName('cells');
+const cellsCount = field.childElementCount;
+const startPos = Math.floor(0 + Math.random() * (cellsCount - 1));
 let currPos;
 function insertPic(position) {
-    do{
-        for (let i = 0;i<cells.length;i++)
-        {
-         if(i===position && position!=currPos)
-         {
-             cells[i].innerHTML = '<img src="https://github.com/netology-code/ahj-homeworks/blob/master/dom/pic/goblin.png?raw=true" alt="goblin" id="goblin" class="goblin">';
-             currPos = position;
-             return;
-            }
-            else {position = Math.floor(0  + Math.random() * (cellsCount-1));}
-        }
+  do {
+    // eslint-disable-next-line no-plusplus
+    for (let i = 0; i < cells.length; i++) {
+      if (i === position && position !== currPos) {
+        cells[i].innerHTML = '<img src="https://github.com/netology-code/ahj-homeworks/blob/master/dom/pic/goblin.png?raw=true" alt="goblin" id="goblin" class="goblin">';
+        currPos = position;
+        return;
+      }
+      // eslint-disable-next-line no-param-reassign
+      position = Math.floor(0 + Math.random() * (cellsCount - 1));
     }
-    while (position!=currPos)
-    
+  }
+  while (position !== currPos);
 }
 insertPic(startPos);
 
-//<img src="https://github.com/netology-code/ahj-homeworks/blob/master/dom/pic/goblin.png?raw=true" alt="goblin" id="goblin" class="goblin">
+// <img src="https://github.com/netology-code/ahj-homeworks/blob/master/dom/pic/goblin.png?raw=true" alt="goblin" id="goblin" class="goblin">
 function changePos() {
-    document.getElementById('goblin').remove();
-    let newPos = Math.floor(0  + Math.random() * (cellsCount-1));
-    console.log(newPos);
-    insertPic(newPos);
-    
+  document.getElementById('goblin').remove();
+  const newPos = Math.floor(0 + Math.random() * (cellsCount - 1));
+  console.log(newPos);
+  insertPic(newPos);
 }
 
 setInterval(changePos, 1000);
-
